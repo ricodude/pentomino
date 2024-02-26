e21bb8511f2d692b025a9c684b0f73d3465a874a
richard <19240432+ricodude@users.noreply.github.com>
2024-02-26 19:40:12 +0000

    Completed initial setup for tile configs

--- pentomino/solve.py
+++ pentomino/solve.py
@@ -29,14 +29,38 @@ def flip_tile(tile):
 
 
 def rebase_tile(tile):
-    """Rebase the tile coords so that lowest square on y-axis is at y=0 and lowest square on x-axis
-    with (new) y=0 has x=0"""
+    """Rebase the tile coords so that lowest square on y-axis is at y=0 and
+    lowest square on x-axis with (new) y=0 has x=0"""
     new_tile = sorted(tile, key=square_key)
     base = new_tile[0]
     return [(square[0] - base[0], square[1] - base[1]) for square in new_tile]
 
 
-def tile_configs(tile_defs):
+def enrich_config(tile_config, x_range):
+    """Return a dictionary with the tile config (placement) details enriched
+    with useful info for placing the tile"""
+    tile_dict = {
+        'xy_config': tile_config,
+        'adds': [square[0] + x_range * square[1] for square in tile_config],
+    }
+
+    all_x = [square[0] for square in tile_config]
+    min_x = min(all_x)
+    if min_x < 0:
+        tile_dict['min_x'] = min_x
+    max_x = max(all_x)
+    if max_x > 0:
+        tile_dict['max_x'] = max_x
+    max_y = max([square[1] for square in tile_config])
+    if max_y > 0:
+        tile_dict['max_y'] = max_y
+
+    return tile_dict
+
+
+def tile_configs(tile_defs, x_range):
+    """Use the tile definitions to create & return all possible configs
+    (placements) for each tile"""
     tiles = [[(0, 0)] + tile for tile in tile_defs]
     all_configs = []
     for tile in tiles:
@@ -55,4 +79,6 @@ def tile_configs(tile_defs):
                 else:
                     break
         all_configs.append(this_tile_configs)
-    return all_configs
+    enriched_configs = [[enrich_config(config, x_range) for config in configs] \
+                        for configs in all_configs]
+    return enriched_configs
