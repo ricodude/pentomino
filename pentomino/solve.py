a252cda3327a97bd0ac3d4ca890f59228c25e765
Richard Sealy <19240432+ricodude@users.noreply.github.com>
2024-02-27 17:53:02 +0000

    Working version - 8.5 mins for sample problem

--- pentomino/solve.py
+++ pentomino/solve.py
@@ -45,7 +45,7 @@ def enrich_tile(tile, x_range):
     placing"""
     tile_dict = {
         'xy_config': tile,
-        'adds': [square[0] + x_range * square[1] for square in tile],
+        'adds': np.array([square[0] + x_range * square[1] for square in tile]),
     }
 
     all_x = [square[0] for square in tile]
@@ -88,21 +88,30 @@ def tile_orientations(tile_defs, x_range):
     return enriched_tiles
 
 
-def square_index(square, xy_range):
+def square_pos(square, xy_range):
     """Return the index of the square given the range"""
     return square[0] + square[1] * xy_range[0]
 
 
+def square_xy(pos, xy_range):
+    """Return the xy coords of the square given the range"""
+    return pos % xy_range[0], int(pos / xy_range[0])
+
+
 def get_next_tile(tile, tile_orients, remaining_tiles):
     """Tile here refers to (tile_num, orient_num):
-    - if tile is None, return first orientation of first tile.
+    - if tile is None, return first orientation of first of the remaining tiles,
+      or None if there are no remaining tiles;
     - if tile is not None, look for next orientation for this tile and return
       that;
     - if there are no more orientations for the tile, replace it and return
       first orientation for next tile;
     - if there are no more tiles, return None."""
     if tile is None:
-        return 0, 0
+        if len(remaining_tiles) == 0:
+            return None
+        new_tile_num = remaining_tiles.popleft()
+        return new_tile_num, 0
 
     tile_num = tile[0]
     next_orient_num = tile[1] + 1
@@ -111,23 +120,65 @@ def get_next_tile(tile, tile_orients, remaining_tiles):
 
     if len(remaining_tiles) == 0 or \
             tile_num > remaining_tiles[len(remaining_tiles) - 1]:
-        remaining_tiles.appendright(tile_num)
+        remaining_tiles.append(tile_num)
         return None
 
     num_rotations = 0
-    while tile_num < remaining_tiles[0]:
+    while tile_num > remaining_tiles[0]:
         remaining_tiles.rotate(-1)
         num_rotations += 1
     new_tile_num = remaining_tiles.popleft()
     remaining_tiles.appendleft(tile_num)
     remaining_tiles.rotate(num_rotations)
+    temp_list = list(remaining_tiles)
+    if sorted(temp_list) != temp_list:
+        print('Problem')
     return new_tile_num, 0
 
 
-def backtrack(placed_tiles, tile_orients, remaining_tiles):
-    """"""
+def backtrack(placed_tiles, tile_orients, squares, remaining_tiles):
+    """Remove the last placed tile and return it together with the position it
+    was placed in"""
     if len(placed_tiles) == 0:
-        return None
+        return 0, None
+    pos, tile = placed_tiles.pop()
+    tile_squares = tile_orients[tile[0]][tile[1]]['adds'] + pos
+    squares[tile_squares] = 0
+    next_tile = get_next_tile(tile, tile_orients, remaining_tiles)
+    if next_tile is None:
+        return backtrack(placed_tiles, tile_orients, squares, remaining_tiles)
+    return pos, next_tile
+
+
+def place_tile(tile, tile_orients, pos, xy_range, squares, placed_tiles):
+    """Try to place the tile in the specified position. If it succeeds, return
+    the new position for next empty square and None. Otherwise return the
+    current position and current tile."""
+    tile_details = tile_orients[tile[0]][tile[1]]
+
+    # Check the tile fits within the xy range
+    x, y = square_xy(pos, xy_range)
+    if 'min_x' in tile_details:
+        if x + tile_details['min_x'] < 0:
+            return pos, tile
+    if 'max_x' in tile_details:
+        if x + tile_details['max_x'] >= xy_range[0]:
+            return pos, tile
+    if 'max_y' in tile_details:
+        if y + tile_details['max_y'] >= xy_range[1]:
+            return pos, tile
+
+    tile_squares = tile_details['adds'] + pos
+
+    # Check all squares that would be covered by the tile are empty
+    if np.sum(squares[tile_squares]) > 0:
+        return pos, tile
+
+    squares[tile_squares] = 1
+    placed_tiles.append((pos, tile))
+    while pos < len(squares) and squares[pos] > 0:
+        pos += 1
+    return pos, None
 
 
 def solve_puzzle(xy_range, filled_squares, tile_defs):
@@ -144,7 +195,7 @@ def solve_puzzle(xy_range, filled_squares, tile_defs):
 
     # Set the specified squares to filled (1)
     for square in filled_squares:
-        squares[square_index(square, xy_range)] = 1
+        squares[square_pos(square, xy_range)] = 1
 
     # Determine all orientations for the tiles
     tile_orients = tile_orientations(tile_defs, xy_range[0])
@@ -159,13 +210,26 @@ def solve_puzzle(xy_range, filled_squares, tile_defs):
     #   list of tuples (pos, tile) as per placed_tiles above.
     pos = 0
     placed_tiles = deque()
-    remaining_tiles = deque(xy_range(len(tile_orients)))
+    remaining_tiles = deque(range(len(tile_orients)))
     solutions = []
 
     curr_tile = None
     while True:
         curr_tile = get_next_tile(curr_tile, tile_orients, remaining_tiles)
         if curr_tile is None:
-            curr_tile = backtrack(placed_tiles, tile_orients, remaining_tiles)
+            pos, curr_tile = backtrack(placed_tiles, tile_orients, squares,
+                                       remaining_tiles)
             if curr_tile is None:
+                print('Tried everything')
                 return solutions
+        pos, curr_tile = place_tile(curr_tile, tile_orients, pos, xy_range,
+                                    squares, placed_tiles)
+        if pos == len(squares):
+            solution = list(placed_tiles)
+            solutions.append(solution)
+            pos, curr_tile = backtrack(placed_tiles, tile_orients, squares,
+                                       remaining_tiles)
+
+
+solve_puzzle((8, 8), [(1, 2), (3, 4), (5, 7), (6, 7)],
+             TILE_DEFS)
